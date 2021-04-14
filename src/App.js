5967e4b4a920ddfb45914a28aa56769596130cbe
amandena <manda.m.johnson@gmail.com>
2021-04-13 23:32:41 -0400

    imported new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Chatbot from 'react-chatbot-kit'
+import ActionProvider from './components/ActionProvider'
+import MessageParser from './components/MessageParser'
+import config from './config'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Chatbot />
+        <Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser}/>
       </header>
     </div>
   )
